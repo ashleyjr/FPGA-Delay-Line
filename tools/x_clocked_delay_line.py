175989685a3a9dbe5731e70197c7afb641601d1f
Ashley J. Robinson <ashley181291@gmail.com>
2022-09-25 21:13:36 +0100

    create plots

--- tools/x_clocked_delay_line.py
+++ tools/x_clocked_delay_line.py
@@ -1,6 +1,7 @@
 import serial
 import time
 import sys
+import matplotlib.pyplot as plt
 
 ser = serial.Serial(
     port='/dev/ttyUSB1',
@@ -14,13 +15,22 @@ ser.flushOutput()
 
 def get():
     rx = int.from_bytes(ser.read(1), byteorder='big')
-    #print(f">{hex(rx)}")
     return rx
 
 def put(tx):
     ser.write(tx.to_bytes(1, byteorder='big'))
-    #print(f"<{hex(tx)}")
 
+rises_x = []
+rises_y = []
+
+diffs_rises_x = []
+diffs_rises_y = []
+
+falls_x = []
+falls_y = []
+
+diffs_falls_x = []
+diffs_falls_y = []
 
 for c in range(256):
     line = []
@@ -38,4 +48,59 @@ for c in range(256):
         if (line[i] == 0) and (line[i+1] == 1):
             rises.append(i)
 
-    print(f"Ctrl {ctrl}, rises {rises}")
+    falls = []
+    for i in range(255):
+        if (line[i] == 1) and (line[i+1] == 0):
+            falls.append(i)
+
+    print(f"Ctrl {ctrl}, rises {rises}, falls {falls}")
+
+    for r in rises:
+        rises_x.append(c)
+        rises_y.append(r)
+
+    for f in falls:
+        falls_x.append(c)
+        falls_y.append(f)
+
+    if len(rises) > 1:
+        diffs_rises_x.append(c)
+        diffs_rises_y.append(rises[1] - rises[0])
+    if len(rises) > 2:
+        diffs_rises_x.append(c)
+        diffs_rises_y.append(rises[2] - rises[1])
+
+    if len(falls) > 1:
+        diffs_falls_x.append(c)
+        diffs_falls_y.append(falls[1] - falls[0])
+    if len(falls) > 2:
+        diffs_falls_x.append(c)
+        diffs_falls_y.append(falls[2] - falls[1])
+
+
+ax = plt.subplot(221)
+ax.scatter(rises_x,rises_y)
+plt.xticks(range(0,256,32))
+plt.xlim(0,256)
+plt.grid()
+
+ax = plt.subplot(223)
+ax.scatter(diffs_rises_x,diffs_rises_y)
+plt.xticks(range(0,256,32))
+plt.xlim(0,256)
+plt.grid()
+
+ax = plt.subplot(222)
+ax.scatter(falls_x,falls_y)
+plt.xticks(range(0,256,32))
+plt.xlim(0,256)
+plt.grid()
+
+ax = plt.subplot(224)
+ax.scatter(diffs_falls_x,diffs_falls_y)
+plt.xticks(range(0,256,32))
+plt.xlim(0,256)
+plt.grid()
+
+plt.savefig("graph.png", dpi=150)
+
