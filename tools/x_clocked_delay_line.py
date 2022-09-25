c87061bb797a3229538ac7a36218041bc437815e
Ashley J. Robinson <ashley181291@gmail.com>
2022-09-25 17:40:59 +0100

    update py

--- tools/x_clocked_delay_line.py
+++ tools/x_clocked_delay_line.py
@@ -21,6 +21,21 @@ def put(tx):
     ser.write(tx.to_bytes(1, byteorder='big'))
     #print(f"<{hex(tx)}")
 
-put(0)
-for i in range(256//8):
-    print(bin(get()))
+
+for c in range(256):
+    line = []
+    put(0)
+    ctrl = get()
+
+    for i in range(256//8):
+        b = get()
+        for j in range(8):
+            line.append(b & 1)
+            b = b >> 1
+
+    rises = []
+    for i in range(255):
+        if (line[i] == 0) and (line[i+1] == 1):
+            rises.append(i)
+
+    print(f"Ctrl {ctrl}, rises {rises}")
