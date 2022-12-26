8e64f21ac29afbc966ff2380978e087a2b1b48b3
Ashley J. Robinson <ashley181291@gmail.com>
2022-12-26 13:33:25 +0000

    rtl

---
+++ submodules/x_micro_sequencer/rtl/x_micro_sequencer.sv
@@ -0,0 +1,18 @@
+module x_micro_sequencer (
+   input    logic          i_clk,
+   input    logic          i_rst,
+   input    logic          i_start,
+   output   logic          o_busy,
+   input    logic          i_wen,
+   input    logic [3:0]    i_wcmd,  
+   input    logic [35:0]   i_wdata,
+   input    logic [8:0]    i_waddr,
+   output   logic [35:0]   o_data
+);
+
+   assign o_busy = 1'd0;
+   assign o_data = 36'd0;
+
+endmodule
+
+
