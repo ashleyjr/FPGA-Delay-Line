13ce7ed6a2d0eb30401ede82d7dc0a64d2f0f6b2
Ashley J. Robinson <ashley181291@gmail.com>
2022-12-29 11:45:33 +0000

    uarts

---
+++ submodules/x_uart_rx/rtl/x_uart_rx.sv
@@ -0,0 +1,133 @@
+module x_uart_rx#(
+   p_clk_hz = 1000000, 
+   p_baud   = 9600
+)(
+   input    logic       i_clk,
+   input    logic       i_nrst,
+   input    logic       i_rx,
+   output   logic       o_valid,    
+   output   logic [7:0] o_data
+);
+ 
+   localparam p_timer_top   = p_clk_hz / p_baud;
+   localparam p_timer_half  = p_timer_top / 2;
+   localparam p_timer_width = $clog2(p_timer_top);
+    
+   typedef enum logic [6:0] {
+      IDLE, START, 
+      A0, A1, A2, A3, A4, A5, A6, A7
+   } sm_uart_t;
+ 
+   logic                      p0_rx;
+   logic                      p1_rx;
+   logic                      p2_rx;
+   logic                      p3_rx;
+
+   logic                      rx_rise;
+   logic                      rx_fall;
+
+   sm_uart_t                  sm_uart_q;
+   sm_uart_t                  sm_uart_d;  
+   sm_uart_t                  sm_uart_inc;  
+   logic                      sm_uart_en;
+   logic                      sm_uart_idle;
+   logic                      sm_uart_start;
+
+   logic                      timer_top;
+   logic                      timer_half;
+   logic                      timer_wrap;
+   logic [p_timer_width-1:0]  timer_inc;
+   logic [p_timer_width-1:0]  timer_d;
+   logic [p_timer_width-1:0]  timer_q;
+   logic                      timer_en;
+
+   logic [7:0]                data_d;
+   logic [7:0]                data_q;
+   logic                      data_en;
+
+   logic                      valid_d;
+   logic                      valid_q;
+
+   ///////////////////////////////////////////////////////////////////
+   // Resync Input
+  
+   assign p0_rx = i_rx;
+
+   always_ff@(posedge i_clk or negedge i_nrst) begin
+      if(!i_nrst) p1_rx <= 'd1;
+      else        p1_rx <= p0_rx;
+   end
+ 
+   always_ff@(posedge i_clk or negedge i_nrst) begin
+      if(!i_nrst) p2_rx <= 'd1;
+      else        p2_rx <= p1_rx;
+   end   
+   
+   always_ff@(posedge i_clk or negedge i_nrst) begin
+      if(!i_nrst) p3_rx <= 'd1;
+      else        p3_rx <= p2_rx;
+   end   
+  
+   assign rx_rise =  p2_rx & ~p3_rx;
+   assign rx_fall = ~p2_rx &  p3_rx;
+
+   ///////////////////////////////////////////////////////////////////
+   // Timer
+
+   assign timer_top  = (timer_q == p_timer_top);
+   assign timer_half = (timer_q == p_timer_half) &
+                       (sm_uart_q == START);
+   assign timer_wrap = timer_top | timer_half;
+   assign timer_inc  = timer_q + 'd1;
+   assign timer_d    = (timer_wrap) ? 'd0 : timer_inc; 
+   assign timer_en   =  ~sm_uart_idle |  
+                       ((sm_uart_idle) & rx_fall);
+
+   always_ff@(posedge i_clk or negedge i_nrst) begin
+      if(!i_nrst)       timer_q <= 'd0;
+      else if(timer_en) timer_q <= timer_d;
+   end
+   
+   ///////////////////////////////////////////////////////////////////
+   // State machine updates
+  
+   assign sm_uart_inc   = sm_uart_q + 'd1; 
+   assign sm_uart_d     = (sm_uart_q == A7) ? IDLE  : sm_uart_inc;
+   assign sm_uart_idle  = (sm_uart_q == IDLE);
+   assign sm_uart_start = (sm_uart_q == START);
+   assign sm_uart_en    = (sm_uart_idle ) ? rx_fall: 
+                          (sm_uart_start) ? timer_half:
+                                            timer_top; 
+
+   always_ff@(posedge i_clk or negedge i_nrst) begin
+      if(!i_nrst)          sm_uart_q <= IDLE;
+      else if(sm_uart_en)  sm_uart_q <= sm_uart_d;
+   end
+  
+   ///////////////////////////////////////////////////////////////////
+   // Ending
+
+   assign valid_d = (sm_uart_q == A7) & timer_top; 
+
+   always_ff@(posedge i_clk or negedge i_nrst) begin
+      if(!i_nrst) valid_q <= 'd0;
+      else        valid_q <= valid_d;
+   end
+
+   assign o_valid = valid_q;
+
+   ///////////////////////////////////////////////////////////////////
+   // Flop RX
+ 
+   assign data_d  = {p2_rx, data_q[7:1]};
+   assign data_en = ~(  (sm_uart_q == IDLE)|
+                        (sm_uart_q == START)) & 
+                      sm_uart_en;
+   always_ff@(posedge i_clk or negedge i_nrst) begin
+      if(!i_nrst)       data_q <= 'd0;
+      else if(data_en)  data_q <= data_d;
+   end
+
+   assign o_data = data_q;
+      
+endmodule
---
+++ submodules/x_uart_tx/rtl/x_uart_tx.sv
@@ -0,0 +1,81 @@
+module x_uart_tx#(
+   p_clk_hz = 12000000, 
+   p_baud   = 115200
+)(
+   input    logic       i_clk,
+   input    logic       i_nrst,
+   input    logic [7:0] i_data,
+   output   logic       o_tx,
+   input    logic       i_valid,
+   output   logic       o_accept
+);
+ 
+   localparam p_timer_top   = p_clk_hz / p_baud;
+   localparam p_timer_width = $clog2(p_timer_top);
+    
+   typedef enum logic [6:0] {
+      IDLE_0, IDLE_1, START, 
+      A0, A1, A2, A3, A4, A5, A6, A7, STOP
+   } sm_uart_t;
+  
+   sm_uart_t                  sm_uart_q;
+   sm_uart_t                  sm_uart_d;  
+   sm_uart_t                  sm_uart_inc;  
+   logic                      sm_uart_en;
+
+   logic                      timer_top;
+   logic [p_timer_width-1:0]  timer_inc;
+   logic [p_timer_width-1:0]  timer_d;
+   logic [p_timer_width-1:0]  timer_q;
+   logic                      timer_en;
+
+   ///////////////////////////////////////////////////////////////////
+   // Timer
+
+   assign timer_top  = (timer_q == p_timer_top);
+   assign timer_inc  = timer_q + 'd1;
+   assign timer_d    = (timer_top) ? 'd0 : timer_inc; 
+   assign timer_en   = (sm_uart_q != IDLE_0);
+
+   always_ff@(posedge i_clk or negedge i_nrst) begin
+      if(!i_nrst)       timer_q <= 'd0;
+      else if(timer_en) timer_q <= timer_d;
+   end
+   
+   ///////////////////////////////////////////////////////////////////
+   // State machine updates
+  
+   assign sm_uart_inc = sm_uart_q + 'd1; 
+   assign sm_uart_d   = (sm_uart_q == STOP    ) ? IDLE_0  : sm_uart_inc;
+   assign sm_uart_en  = (sm_uart_q == IDLE_0) ? i_valid : timer_top; 
+ 
+   always_ff@(posedge i_clk or negedge i_nrst) begin
+      if(!i_nrst)          sm_uart_q <= IDLE_0;
+      else if(sm_uart_en)  sm_uart_q <= sm_uart_d;
+   end
+  
+   ///////////////////////////////////////////////////////////////////
+   // Ending
+
+   assign o_accept = (sm_uart_q == STOP) & timer_top; 
+
+   ///////////////////////////////////////////////////////////////////
+   // Drive TX
+   
+   always_comb begin
+      case(sm_uart_q) 
+         START:   o_tx = 1'b0;      
+         A0:      o_tx = i_data[0];
+         A1:      o_tx = i_data[1];
+         A2:      o_tx = i_data[2];
+         A3:      o_tx = i_data[3];
+         A4:      o_tx = i_data[4];
+         A5:      o_tx = i_data[5];
+         A6:      o_tx = i_data[6];
+         A7:      o_tx = i_data[7];
+         default: o_tx = 1'b1;
+      endcase
+   end
+ 
+   
+endmodule
